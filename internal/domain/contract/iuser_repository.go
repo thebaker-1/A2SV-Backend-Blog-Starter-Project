0360e5a8a50a9644f34f68a2c35533cc65afc301
Estifanos <learnestifanos@gmail.com>
2025-08-01 12:27:09 +0300

    Add IUserRepository interface with user management methods

--- internal/domain/contract/iuser_repository.go
+++ internal/domain/contract/iuser_repository.go
@@ -1,1 +1,14 @@
 package contract
+
+import (
+	"blog/internal/domain/entity"
+	"context"
+)
+
+type IUserRepository interface {
+	CreateUser(ctx context.Context, user entity.User) (*entity.User, error)
+	GetUserByID(ctx context.Context, id string) (*entity.User, error)
+	GetByUserName(ctx context.Context, username string) (*entity.User, error)
+	UpdateUser(ctx context.Context, user entity.User) (*entity.User, error)
+	DeleteUser(ctx context.Context, id string) error
+}
